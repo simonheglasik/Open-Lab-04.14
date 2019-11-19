b4ea08a1be8c6f602ceebb0d8405e3cc79949c2b
simonheglasik <simonheglasik@gmail.com>
2019-11-19 19:14:15 +0100

    04.14 ✓

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -5,8 +5,32 @@ namespace Open_Lab_04._14
     public class FramePrinter
     {
         public void Print(string[] strings)
-        {
-            throw new NotImplementedException();
+            {
+            string stars = "";
+            string longest = "";
+            string word = "";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (strings[i].Length > longest.Length)
+                {
+                    longest = strings[i];
+                }
+            }
+            for (int o = 0; o < longest.Length + 4; o++)
+            {
+                stars = stars + "*";
+            }
+            Console.WriteLine(stars);
+            foreach (string s in strings)
+            {
+                word = s;
+                for (int o = 0; o < (stars.Length - 4) - s.Length; o++)
+                {
+                    word = word + " ";
+                }
+                Console.WriteLine($"* {word} *");
+            }
+            Console.WriteLine(stars);
         }
     }
 }
